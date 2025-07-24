8acdf14832394365a9dce6e76a30356e79d3ff92
PleD1k <pledik.moy@gmail.com>
2025-07-24 22:49:11 +0300

    DZ 29 cypress

---
+++ cypress/support/commands.js
@@ -0,0 +1,35 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('visitWithTimeout', (url = '/') => { 
+  cy.visit(url, {
+    failOnStatusCode: false,
+    timeout: 60000,
+    loadTimeout: 0,
+    onBeforeLoad(win) { //это все потому что сайт в постоянной подгрузке
+      setTimeout(() => win.stop(), 5000);
+    },
+  });
+});
